59543ad753afb12a2187e1fb085dac07064c7f16
gAlmeida11 <gustavo_almeida11@hotmail.com>
2019-03-28 17:55:06 -0300

    Algoritmo de Pell

---
+++ algoritomoPell.s
@@ -0,0 +1,30 @@
+.data
+	msg1:.asciiz"\nDigite um numero: "
+	msg2:.asciiz"\nResultado: "
+.text
+main:
+	#escrever na tela
+	li $v0,4
+	la $a0, msg1
+	syscall
+	#ler numero
+	li $v0,5
+	syscall
+	add $t0,$v0,$zero
+	#n == 0
+	li $t2,0
+	#i == 1
+	li $t3, 1
+	#estrutra de repetição
+	enquanto:
+	sub $t0,$t0,$t3
+	add $t3,$t3,2
+	add $t2,$t2,1
+	bge $t0,$t3,enquanto
+	#apresentar resultado
+	li $v0,4
+	la $a0, msg2
+	syscall
+	li $v0,1
+	add $a0, $t2, $zero
+	syscall
